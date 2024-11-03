c3a46c849976f9778841bcc59cbd9981e0c97f61
Long Van <longvan121996@gmail.com>
2024-11-03 18:48:42 +0900

    add component

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -26,37 +26,45 @@ export function HeroSection({coupleInfo, weddingInfo}: HeroProps) {
                         <p className="font-bold leading-tight mb-4 animate__animated animate__slideInDown animate__delay-3s">&</p>
                         <p className="font-bold leading-tight mb-4 w-28 sm:w-auto animate__animated animate__slideInRight animate__delay-3s">{coupleInfo.female.fullName}</p>
                     </div>
-                    <div className="text-base sm:text-lg md:text-2xl text-gray-300 mb-12 sm:mb-20 tracking-[5px] border-t-2 border-b-2 py-3 px-5 animate__animated animate__fadeIn animate__delay-4s">
+                    <div
+                        className="text-base sm:text-lg md:text-2xl text-gray-300 mb-12 sm:mb-20 tracking-[5px] border-t-2 border-b-2 py-3 px-5 animate__animated animate__fadeIn animate__delay-4s">
                         <p className="typing-animation typing-animation_delay-4s">
                             WE&#39;RE GETTING MARRIED
                         </p>
                     </div>
                     <div className="relative flex space-x-5 sm:space-x-10 lg:space-x-20 font-serif ">
-                        <div className="relative animate__animated animate__pulse animate__infinite">
-                            <div
-                                className="heart bg-opacity-40 transform scale-100 sm:scale-[1.3] lg:scale-[1.4]"></div>
-                            <div
-                                className="absolute w-full h-full -top-2.5 left-0 flex flex-col justify-center items-center">
-                                <p className="text-sm sm:text-base text-white font-semibold">Ngày</p>
-                                <p className="text-xl sm:text-3xl text-white font-bold">{getDay(weddingInfo.weddingDate)}</p>
+                        <div className="flex animate__animated animate__jackInTheBox animate__delay-3s">
+                            <div className="relative animate__animated animate__pulse animate__infinite">
+                                <div
+                                    className="heart bg-opacity-40 transform scale-100 sm:scale-[1.3] lg:scale-[1.4]"></div>
+                                <div
+                                    className="absolute w-full h-full -top-2.5 left-0 flex flex-col justify-center items-center">
+                                    <p className="text-sm sm:text-base text-white font-semibold">Ngày</p>
+                                    <p className="text-xl sm:text-3xl text-white font-bold">{getDay(weddingInfo.weddingDate)}</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="relative animate__animated animate__pulse animate__infinite">
-                            <div
-                                className="heart bg-opacity-40 transform scale-100 sm:scale-[1.3] lg:scale-[1.4]"></div>
-                            <div
-                                className="absolute w-full h-full -top-2.5 left-0 flex flex-col justify-center items-center">
-                                <p className="text-sm sm:text-base text-white font-semibold">Tháng</p>
-                                <p className="text-xl sm:text-3xl text-white font-bold">{getMonth(weddingInfo.weddingDate)}</p>
+
+                        <div className="flex animate__animated animate__jackInTheBox animate__delay-4s">
+                            <div className="relative animate__animated animate__pulse animate__infinite">
+                                <div
+                                    className="heart bg-opacity-40 transform scale-100 sm:scale-[1.3] lg:scale-[1.4]"></div>
+                                <div
+                                    className="absolute w-full h-full -top-2.5 left-0 flex flex-col justify-center items-center">
+                                    <p className="text-sm sm:text-base text-white font-semibold">Tháng</p>
+                                    <p className="text-xl sm:text-3xl text-white font-bold">{getMonth(weddingInfo.weddingDate)}</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="relative animate__animated animate__pulse animate__infinite">
-                            <div
-                                className="heart bg-opacity-40 transform scale-100 sm:scale-[1.3] lg:scale-[1.4]"></div>
-                            <div
-                                className="absolute w-full h-full -top-2.5 left-0 flex flex-col justify-center items-center">
-                                <p className="text-sm sm:text-base text-white font-semibold">Năm</p>
-                                <p className="text-xl sm:text-3xl text-white font-bold">{getYear(weddingInfo.weddingDate)}</p>
+                        <div className="flex animate__animated animate__jackInTheBox animate__delay-5s">
+                            <div className="relative animate__animated animate__pulse animate__infinite">
+                                <div
+                                    className="heart bg-opacity-40 transform scale-100 sm:scale-[1.3] lg:scale-[1.4]"></div>
+                                <div
+                                    className="absolute w-full h-full -top-2.5 left-0 flex flex-col justify-center items-center">
+                                    <p className="text-sm sm:text-base text-white font-semibold">Năm</p>
+                                    <p className="text-xl sm:text-3xl text-white font-bold">{getYear(weddingInfo.weddingDate)}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
